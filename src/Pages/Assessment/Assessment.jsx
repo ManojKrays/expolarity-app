9a1ee1da8e5f112dc55294a16896705d17ad2802
ManojKrays <manoj@kraysinfotech.com>
2025-10-23 11:36:43 +0530

    commit

--- src/Pages/Assessment/Assessment.jsx
+++ src/Pages/Assessment/Assessment.jsx
@@ -97,7 +97,6 @@ const Assessment = () => {
             </div>
             <div className="flex">
                 <div
-                    Z
                     className={`fixed z-50 h-[100dvh] max-w-xs bg-[#12703C] shadow-lg transition-transform duration-300 md:static md:flex md:w-[20%] md:translate-x-0 ${
                         sidebarOpen ? "translate-x-0" : "-translate-x-full"
                     }`}
